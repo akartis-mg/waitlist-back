66da0f365bc76b2e1edbaaaf99356406a924a306
megane-flow-corporation <megane@team-flow-corporation.com>
2021-07-31 18:29:19 +0400

    auth OK

--- controller/auth.js
+++ controller/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/Users');
 const sendEmail = require('../utils/sendEmail');
@@ -120,6 +121,41 @@ exports.forgotpassword = async (req, res, next) => {
 
 			return next(new ErrorResponse("Email could not be sent", 500));
 		}
+	} catch (error) {
+		next(error);
+	}
+}
+
+// @route    POST api/auth
+// @desc     Reset password
+// @access   Public
+exports.resetpassword = async (req, res, next) => {
+	const resetPasswordToken = crypto
+		.createHash("sha256")
+		.update(req.params.resetToken)
+		.digest("hex");
+
+	try {
+		const user = await User.findOne({
+			resetPasswordToken,
+			resetPasswordExpire: { $gt: Date.now() }
+		})
+
+		if (!user) {
+			return next(new ErrorResponse("Invalid Reset Token", 400));
+		}
+
+		user.password = req.body.password;
+		user.resetPasswordToken = undefined;
+		user.resetPasswordExpire = undefined;
+
+		await user.save();
+
+		res.status(201).json({
+			success: true,
+			data: "Password Reset Success"
+		})
+
 	} catch (error) {
 		next(error);
 	}
