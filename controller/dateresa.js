aad00ad7694f0bf815571077e9778e8758ebb7ff
tina  tantely <tantely@team-flow-corporation.com>
2021-09-01 17:09:43 +0400

    date resa

--- controller/dateresa.js
+++ controller/dateresa.js
@@ -1,3 +1,22 @@
 const Branch = require('../models/Branch');
 const Staff = require('../models/Staff');
 const Reservation = require('../models/Reservation');
+const Dateresa = require('../models/Dateresa');
+
+
+exports.createDateresa = async (req, res, next) => {
+
+    const bid = req.body.bid;
+    const dateresa = new Branch(req.body.dateresa);
+    dateresa.bid=bid;
+   
+    try {
+        const saveddateresa = await dateresa.save();
+
+        res.status(200).json(saveddateresa);
+    }
+
+    catch (error) {
+        next(error);
+    }
+}
--- routes/dateresa.js
+++ routes/dateresa.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { createDateresa ,  getOneDateresa , getDateresa , updateDateresa  } = require("../controller/dateresa.js");
+const { createDateresa ,   getDateresa , updateDateresa  } = require("../controller/dateresa.js");
 
 const router = express.Router();
 const  auth  = require('../middleware/auth');
 
 router.route("/newDateresa").post( auth , createDateresa);
-router.route("/findOneDateresa").get( auth , getOneDateresa);
-router.route("/findAllDateresa").get( auth , getDateresa);
+router.route("/findDateresa").get( auth , getDateresa);
 router.route("/updateDateresa").put(auth ,  updateDateresa);
 
 module.exports = router;
