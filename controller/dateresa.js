ee3c84b3e263422ffa2b893700745bd462490c08
tina  tantely <tantely@team-flow-corporation.com>
2021-09-21 18:30:01 +0400

    edit

--- controller/dateresa.js
+++ controller/dateresa.js
@@ -2,7 +2,7 @@ const Branch = require('../models/Branch');
 const Staff = require('../models/Staff');
 const Reservation = require('../models/Reservation');
 const Dateresa = require('../models/Dateresa');
-
+const moment = require('moment');
 
 exports.getDateresa = async (req, res, next) => {
 
@@ -11,6 +11,82 @@ exports.getDateresa = async (req, res, next) => {
     try {
         const dateresa = await Dateresa.find({ bid: bid })
         res.status(200).json(dateresa);
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+
+exports.getAvailableTimes = async (req, res, next) => {
+
+    const bid = req.params.bid;
+    const jour =  req.params.jour;
+    const daySelected =  moment(req.params.daySelected).format("DD/MM/YYYY") ;
+    let intervalHours = [];
+    let timesAvailable = [];
+
+    console.log("date",daySelected);
+
+    try {
+        const dateresa = await Dateresa.findOne({ bid: bid , "info.date" : daySelected    })
+        const branch = await Branch.findOne({  _id : bid  })
+
+        //get available hour for day clicked
+        branch.info.opening_days[jour].hour_interval[0].map((h) => {
+            timesAvailable.push({
+              hours: h.hours,
+              seats: branch.spots.available,
+            });
+          });
+
+
+
+          console.log("date",dateresa);
+
+          if(dateresa){
+
+                console.log("eo");
+
+                    dateresa.info
+                    .map((ht) => {
+                    console.log("taken ", ht.interval);
+                    intervalHours = ht.interval;
+                    });
+
+                
+
+                if (intervalHours.length > 0) {
+                
+
+                    for (var i = 0; i < timesAvailable.length; i++) {
+                    for (var j = 0; j < intervalHours.length; j++) {
+                        //compare hours from default and taken
+
+                        //console.log("ta", timesAvailable[i].hours);
+                        //console.log("ih", intervalHours[j].hours);
+                        if (timesAvailable[i].hours == intervalHours[j].hours) {
+                        if (branch.spots.available == intervalHours[j].seats) {
+                            console.log("tonga eto", i);
+                            //timesAvailable.filter((item) => item != timesAvailable[i]);
+                            timesAvailable.splice(i, 1);
+                        } else {
+                            //set new seats available
+                            timesAvailable[i].seats =
+                            branch.spots.available - intervalHours[j].seats;
+                        }
+                        }
+                    }
+                    }
+                }
+
+          }
+        
+
+
+        console.log("timesAvailable" , timesAvailable );  
+
+        res.status(200).json(timesAvailable);
     } catch (error) {
         next(error);
     }
--- routes/dateresa.js
+++ routes/dateresa.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getDateresa, updateDateresa } = require("../controller/dateresa.js");
+const { getDateresa , getAvailableTimes } = require("../controller/dateresa.js");
 
 const router = express.Router();
 const auth = require('../middleware/auth');
 
 
 router.route("/findDateresa/:bid").get(auth, getDateresa);
-//router.route("/updateDateresa").put(auth, updateDateresa);
+router.route("/getAvailableTimes/:bid/:jour/:daySelected").get(getAvailableTimes);
 
 module.exports = router;
